4ed883b7480a83e17ef209ec2265ba5c9d288f71
XavierPainchaud <xav.painchaud@outlook.com>
2024-12-22 14:53:20 -0500

    begining of receip

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/grocery_list.json')
-def serve_json():
-    return jsonify({
-        "items": [
-            {"name": "Milk", "specification": "1L"},
-            {"name": "Bread", "specification": "Baguette"},
-            {"name": "Apples", "specification": "6 pieces"}
-        ]
-    })
-
 @app.route('/inventory.json')
-def inventory():
-    df = pd.read_excel('C:\grocery_inventory\inventaire.xlsx')
-    return df
+def inventory_json():
+    pass
+    '''
+    # Read data from the Excel file
+    data = pd.read_excel('C:\grocery\inventaire.xlsx')
 
+    # Convert the data to JSON
+    data_json = data.to_dict(orient='records')
+    return data_json
+    '''
+@app.route('/grocery_list.json')
+def grocery_list_json():
+    inventory = pd.read_excel('C:\\github_repo\\automated_grocery_list\\grocery\\inventaire.xlsx')
+
+    recettes = pd.read_excel('C:\\github_repo\\automated_grocery_list\\grocery\\recettes.xlsx')
+
+    ingredients = recettes['Ingrédients']
+    
+    recettes_json = recettes.to_dict(orient='records')
+
+    return recettes_json
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
