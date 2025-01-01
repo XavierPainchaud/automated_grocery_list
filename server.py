b5a09e3cc151c579a2f3a8bb7af496d0f4f83a37
XavierPainchaud <xav.painchaud@outlook.com>
2025-01-01 11:38:05 -0500

    updates for test

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 import sqlite3
-import json
 
 # Connexion à SQLite
 def get_data():
@@ -18,8 +17,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def bring_list():
-    shopping_list = []
-    json_ld = None  # Par défaut, aucune donnée JSON-LD
+    shopping_list = []  # Définir une liste vide par défaut
 
     if request.method == 'POST':
         week = request.form.get('week', type=int)
@@ -30,25 +28,12 @@ def bring_list():
         shopping_list = generate_shopping_list(week)
 
         if not shopping_list:
-            return render_template('index.html', shopping_list=[], week=week, json_ld=json_ld)
+            return render_template('index.html', shopping_list=[], week=week)
 
-        # Générer JSON-LD
-        json_ld = {
-            "@context": "https://schema.org",
-            "@type": "Recipe",
-            "name": "Liste d'épicerie",
-            "author": {
-                "@type": "Person",
-                "name": "Xavier Painchaud"
-            },
-            "description": "Une application pour générer automatiquement une liste d'épicerie hebdomadaire.",
-            "image": "https://static.vecteezy.com/ti/vecteur-libre/p2/2056660-panier-et-une-liste-de-produits-vectoriel.jpg",
-            "recipeIngredient": shopping_list
-        }
+        return render_template('index.html', shopping_list=shopping_list, week=week)
 
-        return render_template('index.html', shopping_list=shopping_list, week=week, json_ld=json.dumps(json_ld))
-
-    return render_template('index.html', shopping_list=shopping_list, json_ld=json_ld)
+    # Affiche uniquement le formulaire si aucune semaine n'est sélectionnée
+    return render_template('index.html', shopping_list=shopping_list)
 
 
 def generate_shopping_list(week):
@@ -89,6 +74,7 @@ def generate_shopping_list(week):
 
     except Exception as e:
         return {"message": f"Erreur dans la génération de la liste d'épicerie: {str(e)}"}, 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
