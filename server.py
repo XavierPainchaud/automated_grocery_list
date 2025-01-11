b215c4463db4ca67acc1295a114fc56277dbeab4
XavierPainchaud <xav.painchaud@outlook.com>
2025-01-11 15:47:20 -0500

    button added to download liste

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, Response
 import pandas as pd
 import json
-import os
 
 def get_data():
     # Charger les données depuis un fichier Excel
@@ -11,60 +10,6 @@ def get_data():
     recipes = pd.read_excel(file_path, sheet_name='recipes')
 
     return inventory, recipes
-
-def save_to_file(shopping_list, week, store):
-
-    # Définir le chemin du fichier dans C:\github_repo
-    repo_path = r"C:\liste_epicerie"
-    if not os.path.exists(repo_path):
-        os.makedirs(repo_path)  # Créer le dossier s'il n'existe pas
-
-    file_path = os.path.join(repo_path, f"liste_epicerie.txt")
-
-    # Écrire la liste dans le fichier
-    with open(file_path, "w", encoding="utf-8") as file:
-        for item in shopping_list:
-            file.write(f"{item}\n")
-    print(f"Liste enregistrée à : {file_path}")
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return redirect('/select_week')
-
-@app.route('/select_week', methods=['GET'])
-def select_week():
-    return render_template('select_week.html')
-
-@app.route('/ingredients', methods=['GET'])
-def bring_list():
-    week = request.args.get('week', type=int)
-    store = request.args.get('store')
-    if not week or not store:
-        return redirect('/select_week')  # Redirection si la semaine ou le magasin n'est pas défini
-
-    shopping_list = generate_shopping_list(week, store)
-
-    if shopping_list:
-        save_to_file(shopping_list, week, store)  # Sauvegarder la liste dans un fichier texte
-
-    # Générer JSON-LD
-    json_ld = {
-        "@context": "https://schema.org",
-        "@type": "Recipe",
-        "name": f"Liste d'épicerie pour {store}, semaine {week}",
-        "author": {
-            "@type": "Person",
-            "name": "Xavier Painchaud"
-        },
-        "description": "Une application pour générer automatiquement une liste d'épicerie hebdomadaire.",
-        "image": "https://static.vecteezy.com/ti/vecteur-libre/p2/2056660-panier-et-une-liste-de-produits-vectoriel.jpg",
-        "recipeIngredient": shopping_list
-    }
-
-    return render_template('index.html', shopping_list=shopping_list, week=week, store=store, json_ld=json.dumps(json_ld))
-
 
 def generate_shopping_list(week, store):
     try:
@@ -103,6 +48,61 @@ def generate_shopping_list(week, store):
     except Exception as e:
         return {"message": f"Erreur dans la génération de la liste d'épicerie: {str(e)}"}, 500
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect('/select_week')
+
+@app.route('/select_week', methods=['GET'])
+def select_week():
+    return render_template('select_week.html')
+
+@app.route('/ingredients', methods=['GET'])
+def bring_list():
+    week = request.args.get('week', type=int)
+    store = request.args.get('store')
+    if not week or not store:
+        return redirect('/select_week')  # Redirection si la semaine ou le magasin n'est pas défini
+
+    shopping_list = generate_shopping_list(week, store)
+
+    # Générer JSON-LD
+    json_ld = {
+        "@context": "https://schema.org",
+        "@type": "Recipe",
+        "name": f"Liste d'épicerie pour {store}, semaine {week}",
+        "author": {
+            "@type": "Person",
+            "name": "Xavier Painchaud"
+        },
+        "description": "Une application pour générer automatiquement une liste d'épicerie hebdomadaire.",
+        "image": "https://static.vecteezy.com/ti/vecteur-libre/p2/2056660-panier-et-une-liste-de-produits-vectoriel.jpg",
+        "recipeIngredient": shopping_list
+    }
+
+    return render_template('index.html', shopping_list=shopping_list, week=week, store=store, json_ld=json.dumps(json_ld))
+
+@app.route('/download', methods=['GET'])
+def download_list():
+    week = request.args.get('week', type=int)
+    store = request.args.get('store')
+
+    # Générer la liste d'épicerie
+    shopping_list = generate_shopping_list(week, store)
+    if not shopping_list:
+        return "Aucune liste disponible.", 404
+
+    # Formatage du fichier
+    file_content = f"\n".join(shopping_list)
+    filename = "liste_epicerie.txt"
+
+    # Retourner le fichier en tant que réponse
+    return Response(
+        file_content,
+        mimetype="text/plain",
+        headers={"Content-Disposition": f"attachment;filename={filename}"}
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
